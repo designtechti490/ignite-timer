a7d1a862caf657aaa13c796300abb5e4829a207d
Marcelo Junior <designtechti.dev@protonmail.com>
2025-03-06 18:02:47 -0300

    :rocket: feat: Header & Layout :rocket: Add Header and DefaultLayout with styles and icons

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+
 import Home from "./pages/Home";
 import History from "./pages/History";
-import DefaultLayout from "./layouts/defaultLayout";
 
 export function Router() {
   return (
