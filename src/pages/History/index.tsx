a808c080938631d102388493e2e0fffa47c9d294
Marcelo Junior <designtechti.dev@protonmail.com>
2025-03-08 22:06:49 -0300

    :art: style: Component Status :art: Implements Visual Status on history list

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export default function History() {
   return (
@@ -19,49 +19,49 @@ export default function History() {
               <td>Formação Ignite</td>
               <td>25 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Formação Ignite</td>
               <td>25 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em Andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Formação Ignite</td>
               <td>25 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
             <tr>
               <td>Formação Ignite</td>
               <td>25 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Formação Ignite</td>
               <td>25 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em Andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Formação Ignite</td>
               <td>25 minutos</td>
               <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Formação Ignite</td>
-              <td>25 minutos</td>
-              <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Formação Ignite</td>
-              <td>25 minutos</td>
-              <td>Há cerca de 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
