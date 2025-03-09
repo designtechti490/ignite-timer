4868d8e35b223b49374301bbc2afd5cc72ee0ff2
Marcelo Junior <designtechti.dev@protonmail.com>
2025-03-08 21:31:56 -0300

    :rocket: feat: Page History :rocket: Add History Page with Task Table List

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,71 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
+export default function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Formação Ignite</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Formação Ignite</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Formação Ignite</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Formação Ignite</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Formação Ignite</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Formação Ignite</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Formação Ignite</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Formação Ignite</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
+}
