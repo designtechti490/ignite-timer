8bd59b966f68a949dfc83ae52e94941700f6d59b
Marcelo Junior <designtechti.dev@protonmail.com>
2025-03-12 18:00:24 -0300

    :rocket: feat: Converting to Context :rocket: Simplifies the Countdown component and implements context for cycle management

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 
-import Home from "./pages/Home";
+import Home from "./pages/Home/";
 import History from "./pages/History";
 
 export function Router() {
